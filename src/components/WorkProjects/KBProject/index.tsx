487f8927cb4e6799bee3b7400eb9377a5dbd4b38
cykim <present1306@naver.com>
2020-11-23 08:10:13 +0900

    work project 내용, css 수정

--- src/components/WorkProjects/KBProject/index.tsx
+++ src/components/WorkProjects/KBProject/index.tsx
@@ -15,14 +15,25 @@ const KBProject = () => {
       </div>
       <div className="projectBox">
         <div className="descriptionBox">
-          <p className="roleTop">작업내용</p>
-          <p className="roleTitle">관리자페이지</p>
-          <p className="description">- QR가맹점통합관리자페이지 화면 기획, 디자인, DB설계, 개발</p>
-
-          <p className="roleTitle">QR코드관리시스템</p>
-          <p className="description">- QR코드 발급, 검증, 폐기, 조회 등 QR코드관리시스템 DB설계, 개발 및 관리자페이지 연동</p>
+          {/* <p>KB카드 QR코드관리시스템구축</p> */}
           <p>기간 : 2020.1~2020.5</p>
           <p className="languages">자바, jsp, 오라클</p>
+
+          <div>
+            <p className="roleTop">담당파트</p>
+            
+            <div className="roleSubBox">
+              <p className="roleMainText">· 관리자페이지</p>
+              <p className="subDescription">- 구축요건정의서에 따라 QR가맹점통합관리자페이지 기획 및 화면설계서 작성, DB설계</p>
+              <p className="subDescription">- 관리자페이지 프론트엔드, 백엔드 개발</p>
+            </div>
+            <div className="roleSubBox">
+              <p className="roleMainText">· QR코드관리시스템</p>
+              <p className="subDescription">- 구축요건정의서, EMV QR코드규격서 및 KB QR코드규격서에 따라 QR코드 관련 DB설계</p>
+              <p className="subDescription">- 고정형과 변동형 QR코드의 발급, 검증, 폐기, 조회 등 QR코드 관리시스템 개발 및 관리자페이지 연동</p>
+            </div>
+          </div>
+
         </div>
         <div id="workProjectImgBox2" className="workProjectImgBox"/>
       </div>
--- src/components/WorkProjects/ROS2Project/index.tsx
+++ src/components/WorkProjects/ROS2Project/index.tsx
@@ -14,17 +14,22 @@ const WorkProject = () => {
           <h2>LG CLOI 로봇 프론트엔드</h2>
         </div>
         <div className="projectBox">
-
+          
           {/* <projectImg image = "kb_qr.jpg" /> */}
           <div id="workProjectImgBox1" className="workProjectImgBox"/>
           <div className="descriptionBox">
-            <p>LG전자 Cloi 길안내로봇 프론트엔드 개발</p>
-            <p className="descriptionLine">- 개발환경 : ubuntu 데스크탑, cordova</p>
-            <p className="roleTop">작업내용</p>
-            <p>- 로봇스크린 화면 퍼블리싱</p>
-            <p>- 사용자 인터랙티브 및 로봇 이벤트들에 대한 UI 동적 업데이트</p>
-            <p>- 시설물 POI data 연동</p>
-            <p>- 기간 : 2019.8~2019.11</p>
+            {/* <p>LG전자 Cloi 길안내로봇 프론트엔드 개발</p> */}
+            <p>기간 : 2019.8~2019.11</p>
+            <p>React, Redux, styled-componnent</p>
+            <p className="descriptionLine">개발환경 : ubuntu 데스크탑, cordova</p>
+
+            <div>
+              <p className="roleTop">담당파트</p>
+              <p className="roleMainText"> · 로봇스크린 화면개발
+              사용자 인터랙티브 및 로봇 이벤트들에 대한 UI 동적 업데이트</p>
+              <p className="roleMainText"> · 시설물 POI data 연동</p>
+            </div>
+            
           </div>
         </div>
       </div>
